6c557f53abb9b564021682bbe5ad63e66ca2eb2d
Austin Balarin <austin@Austins-MacBook-Pro.local>
2016-11-10 12:37:24 -0500

    added simple base64 encoding to url

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,22 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { URL: 'sample' });
+  res.render('index');
 });
 
 router.post('/', function(req, res, next) {
-  console.log('the json', req.body);
-  res.render('index', req.body);
+  //console.log('the json', req.body);
+
+  //doing a 64 base encoding proc on entered URL
+  var url = req.body.URL;
+  var encoded = new Buffer(url).toString('base64');
+
+  //Storing encoded url into JSON and passing it to html page.
+  // next step: shorten encoding probably using some bit shiffting
+  // and padding ends to fit in 3 char key.
+  url64 = 'www.example.com/' + encoded;
+  enc = {URL : url64}
+  res.render('index', enc);
 });
 
 module.exports = router;
