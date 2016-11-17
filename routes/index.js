1b9e5eae064481b4da3adc767001ad6cd721b625
Austin Balarin <austin@Austins-MacBook-Pro.local>
2016-11-17 11:37:34 -0500

    slight changes to index.js

--- routes/index.js
+++ routes/index.js
@@ -19,47 +19,47 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/', function(req, res, next) {
-<<<<<<< HEAD
-  //console.log('the json', req.body);
-
-  //doing a 64 base encoding proc on entered URL
-  var url = req.body.URL;
-  base62(url);
-  var encoded = new Buffer(url).toString('base64');
-  var url = parseInt(encoded, 10);
-  //console.log(encoded);
-  //var shorten = new Buffer(url).toString('base64');
-  console.log(encoded);
-  console.log(base10);
-  for (i=0; i < encoded.length; i++){
-    var c = encoded[i];
-  }
-
-
-  //Storing encoded url into JSON and passing it to html page.
-  // next step: shorten encoding probably using some bit shiffting
-  // and padding ends to fit in 3 char key.
-  //shorten = encoded.trunc(5)
-  //console.log(shorten);
-  url64 = 'www.example.com/' + encoded;
-  enc = {URL : url64}
-  res.render('index', enc);
-});
-
-function base10(){
-
-}
-module.exports = router;
-=======
+// <<<<<<< HEAD
+//   //console.log('the json', req.body);
+//
+//   //doing a 64 base encoding proc on entered URL
+//   var url = req.body.URL;
+//   base62(url);
+//   var encoded = new Buffer(url).toString('base64');
+//   var url = parseInt(encoded, 10);
+//   //console.log(encoded);
+//   //var shorten = new Buffer(url).toString('base64');
+//   console.log(encoded);
+//   console.log(base10);
+//   for (i=0; i < encoded.length; i++){
+//     var c = encoded[i];
+//   }
+//
+//
+//   //Storing encoded url into JSON and passing it to html page.
+//   // next step: shorten encoding probably using some bit shiffting
+//   // and padding ends to fit in 3 char key.
+//   //shorten = encoded.trunc(5)
+//   //console.log(shorten);
+//   url64 = 'www.example.com/' + encoded;
+//   enc = {URL : url64}
+//   res.render('index', enc);
+// });
+//
+// function base10(){
+//
+// }
+// module.exports = router;
+// =======
     // get the tiny url
-    
-    
+
+
     // create a new object to store in the database
     var newUrl = new url();
     newUrl.url = req.body.URL;
     // this should be the tiny URL
     newUrl.tiny_url = req.body.URL;
-    
+
     // save the object to the DB and report any error
     newUrl.save(function (error){
         if (error){
@@ -73,4 +73,3 @@ module.exports = router;
 });
 
 module.exports = router;
->>>>>>> 28d7b7943b2546b05088b276bda8c89ba712bb40
