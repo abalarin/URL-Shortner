8b7878bc6675867ec3768f415210f4580a6ea1fb
Austin Balarin <austin@Austins-MacBook-Pro.local>
2016-11-17 13:09:45 -0500

    fixed index.js

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,6 @@
+var express = require('express');
+var router = express.Router();
+
 var base62 = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 console.log(base62.length);
   var s = 'asjdnfjasnrjl4r';
@@ -66,7 +69,7 @@ router.post('/', function(req, res, next) {
     });
    console.log('the json', req.body.URL);
    res.render('index', req.body);
-  
+
 });
 
 module.exports = router;
